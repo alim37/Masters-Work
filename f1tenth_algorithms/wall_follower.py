b876effeea83632752f5322968b1831c4d79e29b
malik ali <158620884+alim37@users.noreply.github.com>
2025-10-23 12:43:08 -0400

    Update wall_follower.py

--- f1tenth_algorithms/wall_follower.py
+++ f1tenth_algorithms/wall_follower.py
@@ -15,7 +15,6 @@ class WallFollowerNode(Node):
         
         self.scan_sub = self.create_subscription(LaserScan, "/scan", self.scan_callback, 10)
         self.drive_pub = self.create_publisher(AckermannDriveStamped, "/drive", 10)
-        self.odom_sub = self.create_subscription(Odometry, '/ego_racecar/odom', self.odom_callback, 10)
 
         self.kp = 3.0
         self.ki = 0.001
@@ -23,12 +22,6 @@ class WallFollowerNode(Node):
 
         self.prev_error = 0.0
         self.integral = 0.0
-        
-        self.velocities = []
-        self.times = []
-        self.start_time = None
-        self.lap_end_time = None
-        self.lap_completed = False
         
     def scan_callback(self, data):
         error = self.calculate_error(data)
@@ -79,52 +72,6 @@ class WallFollowerNode(Node):
             drive_msg.drive.speed = 1.5
 
         self.drive_pub.publish(drive_msg)
-    
-    def odom_callback(self, msg):
-        if self.start_time is None:
-            self.start_time = self.get_clock().now()
-
-        current_time = (self.get_clock().now() - self.start_time).nanoseconds / 1e9
-        v_x = msg.twist.twist.linear.x
-        v_y = msg.twist.twist.linear.y
-        total_velocity = math.sqrt(v_x ** 2 + v_y ** 2)
-
-        self.velocities.append(total_velocity)
-        self.times.append(current_time)
-        
-        current_x = msg.pose.pose.position.x
-        current_y = msg.pose.pose.position.y
-        
-        if current_x <= 1 and current_y <= 1 and not self.lap_completed:
-            lap_time = (self.get_clock().now() - self.start_time).nanoseconds / 1e9
-            self.lap_completed = True
-            self.get_logger().info(f'Lap completed in {lap_time:.2f} seconds')
-        
-        if current_x and current_y <= 1:
-            self.lap_completed = False
-            
-    def save_data(self):
-        save_dir = os.path.expanduser('~/data_pid')
-        os.makedirs(save_dir, exist_ok=True)
-        
-        # Save velocities
-        velocities_path = os.path.join(save_dir, 'velocities_pid.csv')
-        with open(velocities_path, 'w', newline='') as f:
-            writer = csv.writer(f)
-            writer.writerow(['Velocity'])  # Header
-            for velocity in self.velocities:
-                writer.writerow([velocity])
-        
-        # Save times
-        times_path = os.path.join(save_dir, 'times_pid.csv')
-        with open(times_path, 'w', newline='') as f:
-            writer = csv.writer(f)
-            writer.writerow(['Time'])  # Header
-            for time in self.times:
-                writer.writerow([time])
-
-        self.get_logger().info(f'Velocities saved to: {velocities_path}')
-        self.get_logger().info(f'Times saved to: {times_path}')
 
 
 def main(args=None):
